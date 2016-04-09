cf75cf73bb9742318c44d53d4b0139738e06604b
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 14:08:21 -0400

    wrong json call

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,6 @@ function myFunction() {
 $(document).ready(function(){
     var apiKey = <%= process.env.API_KEY %>;
     $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=' + apiKey, function(data) {
-        $('#content').append(data.city.name);
+        $('#content').append(data.sys.name);
     })
 })
