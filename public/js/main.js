60cd8619460f722fbe38509d1645d766901bc934
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 14:22:15 -0400

    revert to old code

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,10 @@
-var apiKey = '<%= process.env.API_KEY %>';
+
+function myFunction() {
+    $('#demo').append('<p>NEWWW PARAGRAPH #javascript #swag</>')
+}
 
 $(document).ready(function(){
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=ff51f31c249d0248530b15e67bb1cd96', function(data) {
-        $('#content').append(data.sys.name);
+    $.get('http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=ff51f31c249d0248530b15e67bb1cd96', function(data) {
+        $('#content').append(data.city.name);
     })
 })
