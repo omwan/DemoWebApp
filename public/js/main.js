0a257fdc7b6d1d8d0669449fee8c572681ceee58
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 13:38:48 -0400

    jquery

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,10 @@
 
 function myFunction() {
-    var node = document.createElement("LI");                
-    var textnode = document.createTextNode("NEWWW PARAGRAPH #javascript #swag");         
-    node.appendChild(textnode);                            
+//    var node = document.createElement("LI");                
+//    var textnode = document.createTextNode("NEWWW PARAGRAPH #javascript #swag");         
+//    node.appendChild(textnode);                            
 //    document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
-    document.getElementById("demo").appendChild(node);
+//    document.getElementById("demo").appendChild(node);
+    $('#name').append('<p>NEWWW PARAGRAPH #javascript #swag</p>')
 }
 
