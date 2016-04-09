e74427b1e74763534aa31ce1426a1f978fe2fe00
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 14:29:43 -0400

    stuck env in top

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,10 @@
 
+var apiKey = '<%= process.env.API_KEY %>';
 function myFunction() {
     $('#demo').append('<p>NEWWW PARAGRAPH #javascript #swag</>')
 }
 
 $(document).ready(function(){
-    var apiKey = '<%= process.env.API_KEY %>';
     $.get('http://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=' + apiKey, function(data) {
         $('#content').text(data.coord.lon);
     })
