b105ab90898becc11ec70d44d986bf1d8db41689
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 14:15:11 -0400

    testing

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
+var apiKey = '<%= process.env.API_KEY %>';
+
 $(document).ready(function(){
-    var apiKey = <%= process.env.API_KEY %>;
+    
+    
     $.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=' + ff51f31c249d0248530b15e67bb1cd96, function(data) {
         $('#content').append(data.sys.name);
     })
