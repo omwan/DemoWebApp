b02a819af611045cd09d0ff8eb62f494751ca57c
Olivia Wan <oliviawan97@gmail.com>
2016-04-09 11:56:25 -0400

    javascript appendChild

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 
 function myFunction() {
-  document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+    var node = document.createElement("LI");                
+    var textnode = document.createTextNode("NEWWW PARAGRAPH #javascript #swag");         
+    node.appendChild(textnode);                            
+//    document.getElementById("demo").innerHTML = "NEWWW PARAGRAPH #javascript #swag";
+    document.getElementById("demo").appendChild(node);
 }
 
